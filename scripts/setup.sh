a44a247185c23995ef7ac4406226d6b65bb411dc
Samuel Fernando Nunes de Azevedo Ramalho <98776019+samramalho@users.noreply.github.com>
2026-02-15 14:27:59 -0300

    Update setup.sh
    Add Samba setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+
+echo "Installing Samba..."
+sudo apt update
+sudo apt install samba -y
+
+echo "Creating shared directory..."
+sudo mkdir -p /srv/samba/shared
+sudo chmod 770 /srv/samba/shared
+
+echo "Creating Samba group..."
+sudo groupadd smbusers
+
+echo "Restarting Samba..."
+sudo systemctl restart smbd
+
+echo "Samba lab setup completed."
 
